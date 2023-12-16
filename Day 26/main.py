0971d8e10b5c9990f089b7a38c65d329eb6effd9
sidharth-99 <sidharthv24@gmail.com>
2023-12-16 17:47:50 +0530

    update

--- Day 26/main.py
+++ Day 26/main.py
@@ -10,6 +10,15 @@ print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        break
+    except KeyError:
+        print('Sorry , only letters in alphabet please')
+
 print(output_list)
+    
+
+
--- Day 29/main.py
+++ Day 29/main.py
@@ -4,6 +4,21 @@ import json
 import random
 # import pyperclip install this to add copy to clipboard for password
 
+# ---------------------------- Search ------------------------------- #
+def search():
+    website = entry1.get()
+    with open('data.json','r') as file:
+        data = json.load(file)
+        try: 
+            message = f'''
+                These are the details
+                Email - {data[website]['Email']}
+                Password - {data[website]['Email']}
+                '''
+            messagebox.showinfo(title="Passowrd Found!!" , message=message)
+        except KeyError:
+            messagebox.showinfo(title="Oops" , message="No Passwords found for this website")
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def show_password():
     password = generate_password()
@@ -34,28 +49,63 @@ def generate_password():
 
 def add_password():
     
-    dict = {}
-    dict["Website"] = entry1.get()
-    dict["Email"] = entry2.get()
-    dict["Password"] = entry3.get()
+    website = entry1.get()
+    email = entry2.get()
+    password = entry3.get()
     
+    new_dict = {
+
+        website:{
+            'Email':email,
+            'Password':password
+        }
+    }
+
     message = f'''
     These are the details added
-    Website - {entry1.get()}
-    Email - {entry2.get()}
-    Password - {entry3.get()}
+    Website - {website}
+    Email - {email}
+    Password - {password}
     Is it OK?
     '''
-    if len(entry1.get())==0 or len(entry2.get())==0:
+    if len(website)==0 or len(email)==0:
         messagebox.showinfo(title="Oops" , message="Please Make sure you haven't left any fields empty")
     else:
         is_ok = messagebox.askokcancel(title=entry1, message=message)
     
         if is_ok:
-            with open("password.txt","a") as file:
-                file.write(json.dumps(dict)+'\n')
-                entry1.delete(0,END)
-                entry3.delete(0,END)
+            # try:
+            #     open("data.json","r")
+            # except FileNotFoundError:
+            #     open("data.json", "w")
+            # with open("data.json","r") as file:#r for reading
+            #     # json.dump(new_dict, file, indent=4) not correct as just appending won't work
+            #     data = json.load(file)
+            #     data.update(new_dict)
+
+            # with open("data.json","w") as file:
+            #     json.dump(data, file, indent=4)
+            #     entry1.delete(0,END)
+            #     entry3.delete(0,END)
+            
+            # Earlier code was wrong
+
+                try:
+                    with open("data.json","r") as file:#r for reading
+                        # json.dump(new_dict, file, indent=4) not correct as just appending won't work
+                        data = json.load(file)
+                except FileNotFoundError:
+                        with open("data.json","w") as file:
+                             json.dump(new_dict,file, indent=4)
+
+                else:    
+                    data.update(new_dict)
+
+                    with open("data.json","w") as file:
+                        json.dump(data, file, indent=4)
+                finally:
+                    entry1.delete(0,END)
+                    entry3.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -71,13 +121,13 @@ canvas.grid(row=0,column=1)
 label1 = Label(text="Website: ",font=('Courier',12, "bold"), bg="white")
 label1.grid(row=1,column=0)
 
-entry1= Entry(width=35)
-entry1.grid(row=1,column=1, columnspan=2)
+entry1= Entry(width=21)
+entry1.grid(row=1,column=1)
 
 label2 = Label(text="Email/Username: ",font=('Courier',12, "bold"), bg="white")
 label2.grid(row=2,column=0)
 
-entry2= Entry(width=35)
+entry2= Entry(width=42)
 entry2.insert(END,string="sidharthv24@gmail.com")
 entry2.grid(row=2,column=1, columnspan=2)
 
@@ -87,10 +137,13 @@ label3.grid(row=3,column=0)
 entry3= Entry(width=21)
 entry3.grid(row=3,column=1)
 
-button1 = Button(text="Generate", command=show_password, bg="white",font=('Courier',10, "bold"))
+button1 = Button(text="Generate", command=show_password, width=15, bg="white",font=('Courier',10, "bold"))
 button1.grid(row=3, column=2)
 
 button2 = Button(text="Add", command=add_password,width=36, bg="white",font=('Courier',10, "bold"))
 button2.grid(row=4, column=1, columnspan=2)
+
+button3 = Button(text="Search", bg="white",command=search, width=15,font=('Courier',10, "bold"))
+button3.grid(row=1, column=2)
 
 window.mainloop()
